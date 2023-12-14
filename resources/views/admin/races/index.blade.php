0376d81a8a7b77ae2bf4d511b002ab972afea5d0
Pirisinu <palazzialessandro2@gmail.com>
2023-12-14 18:28:08 +0100

    [ add ] Race delete() button

--- resources/views/admin/races/index.blade.php
+++ resources/views/admin/races/index.blade.php
@@ -18,9 +18,18 @@
                 <td>{{$race->name}}</td>
                 <td>{{$race->description}}</td>
                 <td>
+                    {{-- SHOW --}}
                     <a class="btn btn-primary" href="{{route('admin.races.show', $race)}}"><i class="fa-solid fa-eye"></i></a>
+                    {{-- EDIT --}}
                     <a class="btn btn-dark" href=""><i class="fa-solid fa-pencil"></i></a>
-                    <a class="btn btn-warning" href=""><i class="fa-solid fa-trash"></i></a>
+                    {{-- DELETE --}}
+                    <form class="d-inline-block" action={{route('admin.races.destroy', $race)}} method="post" onsubmit="return confirm('Are you sure you want to delete this race?')">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </form>
                 </td>
             </tr>
         @endforeach
