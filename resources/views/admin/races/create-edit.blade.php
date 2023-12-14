47d149ab631c1278a1579a35b8394e90d8b6d2f1
Pirisinu <palazzialessandro2@gmail.com>
2023-12-14 15:36:18 +0100

    [ add ] Race views

---
+++ resources/views/admin/races/create-edit.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+
+   <h1>Form create-edit</h1>
+
+@endsection
---
+++ resources/views/admin/races/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Races list:</h1>
+@endsection
