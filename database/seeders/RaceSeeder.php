5f571bfa3808cae4db23a9ccdf4c8b77a1c23d17
Pirisinu <palazzialessandro2@gmail.com>
2023-12-14 15:42:25 +0100

    [ add ] RaceSeeder

---
+++ database/seeders/RaceSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class RaceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker)
+    {
+
+    }
+}
