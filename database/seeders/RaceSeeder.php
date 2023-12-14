c05a58cc565b56c1adde36c4060c5499367a9833
Pirisinu <palazzialessandro2@gmail.com>
2023-12-14 16:09:31 +0100

    [ add ] RaceSeeder races data

--- database/seeders/RaceSeeder.php
+++ database/seeders/RaceSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Race;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
 
 class RaceSeeder extends Seeder
 {
@@ -13,6 +15,16 @@ class RaceSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-
+        $races = config('races');
+        foreach ($races as $race) {
+            $race['slug'] = Race::generateSlug($race['name'], Race::class);
+            $race['Mod_str'] = $faker->numberBetween(-3, 3);
+            $race['Mod_dex'] = $faker->numberBetween(-3, 3);
+            $race['Mod_con'] = $faker->numberBetween(-3, 3);
+            $race['Mod_int'] = $faker->numberBetween(-3, 3);
+            $race['Mod_wis'] = $faker->numberBetween(-3, 3);
+            $race['Mod_cha'] = $faker->numberBetween(-3, 3);
+            DB::table('races')->insert($race);
+        }
     }
 }
