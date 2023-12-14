6ad8404a33ebe407976640f4b6a543ce51073a81
Pirisinu <palazzialessandro2@gmail.com>
2023-12-14 15:49:39 +0100

    [ add ] file config->races with ['name','description']

---
+++ config/races.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+
+    [
+        'name' => 'Humans',
+        'description' => 'Versatile and adaptable, humans are found in many cultures and regions in the world of D&D.',
+    ],
+
+    [
+        'name' => 'Elves',
+        'description' => 'Agile and magic-inclined, elves are divided into subraces such as high elves, wood elves, and dark elves.',
+    ],
+
+    [
+        'name' => 'Dwarves',
+        'description' => 'Skilled craftsmen and armored warriors, dwarves live in mountains and are known for their resilience.',
+    ],
+
+    [
+        'name' => 'Half-Elves',
+        'description' => 'Descendants of humans and elves, half-elves inherit traits from both races.',
+    ],
+
+    [
+        'name' => 'Half-Orcs',
+        'description' => 'Resulting from a union between humans and orcs, half-orcs often exhibit strength and brutality.',
+    ],
+
+    [
+        'name' => 'Halflings',
+        'description' => 'Similar to humans but smaller in stature, halflings are known for their agility and luck.',
+    ],
+
+    [
+        'name' => 'Gnomes',
+        'description' => 'Curious and skilled in magic, gnomes are often artisans and scholars.',
+    ],
+
+    [
+        'name' => 'Dragonborn',
+        'description' => 'Descendants of dragons, dragonborn have draconic characteristics and are often heavily armored warriors.',
+    ],
+
+    [
+        'name' => 'Tieflings',
+        'description' => 'Descendants of demons or other infernal entities, tieflings exhibit demonic traits and often feel marginalized.',
+    ],
+
+    [
+        'name' => 'Goblins',
+        'description' => 'Grotesque and cunning creatures similar to orcs, often associated with a crafty mentality.',
+    ],
+
+    [
+        'name' => 'Orcs',
+        'description' => 'Strong and savage, orcs are often nomadic warriors or soldiers in service to warlords.',
+    ],
+
+    [
+        'name' => 'Aarakocra',
+        'description' => 'Bird-like creatures with wings, inhabitants of mountainous and windy regions.',
+    ],
+
+    [
+        'name' => 'Genasi',
+        'description' => 'Individuals with elemental traits, such as fire, earth, air, or water, stemming from connections with elemental beings.',
+    ],
+
+    [
+        'name' => 'Tabaxi',
+        'description' => 'Feline humanoid beings, known for their agility and curious nature.',
+    ],
+
+    [
+        'name' => 'Kenku',
+        'description' => 'Crow-like humanoids known for their ability to mimic sounds and voices.
+        ',
+    ],
+
+];
