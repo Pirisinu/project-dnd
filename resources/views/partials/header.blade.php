0f4ccbfcc1ec4427a59fdc33a91c99cd36832f1a
Pirisinu <palazzialessandro2@gmail.com>
2023-12-15 15:48:50 +0100

    [ add ] header scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header id="admin-header" class="h-100">
+<header id="admin-header">
     <nav class="d-flex justify-content-between">
         <button class="btn btn-light"><a href="{{ route('dashboard')}}" class="navbar-brand">Home <i class="fa-solid fa-house fa-flip"></i></a></button>
         <form class="form-inline" method="POST" action="{{ route('logout')}}">
