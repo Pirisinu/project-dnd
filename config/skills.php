fb307662e7726dfa8eff501babe0a29b870ba3dd
VeronicaFontana <fontana.veronica98@gmail.com>
2023-12-15 15:24:01 +0100

    [Mod] Skills array language

--- config/skills.php
+++ config/skills.php
@@ -1,88 +1,89 @@
 <?php
 
-        return [
-            [
-                'name' => 'Acrobazia',
-                'description' => 'Capacità di eseguire acrobazie e movimenti agili.',
-                'peculiar_characteristic' => 'DEX',
-            ],
-            [
-                'name' => 'Furtività',
-                'description' => 'Capacità di muoversi silenziosamente e rimanere nascosti.',
-                'peculiar_characteristic' => 'DEX',
-            ],
-            [
-                'name' => 'Storia',
-                'description' => 'Conoscenza di eventi passati, culture e società.',
-                'peculiar_characteristic' => 'INT',
-            ],
-            [
-                'name' => 'Sopravvivenza',
-                'description' => 'Abilità nel trovare cibo, acqua e orientarsi nella natura.',
-                'peculiar_characteristic' => 'WIS',
-            ],
-            [
-                'name' => 'Intimidire',
-                'description' => 'Capacità di spaventare o minacciare gli altri.',
-                'peculiar_characteristic' => 'CHA',
-            ],
-            [
-                'name' => 'Inganno',
-                'description' => 'Abilità nel mentire o nascondere la verità.',
-                'peculiar_characteristic' => 'CHA',
-            ],[
-                'name' => 'Arcano',
-                'description' => 'Conoscenza di segreti magici, oggetti magici, e simboli magici.',
-                'peculiar_characteristic' => 'WIS',
-            ],
-            [
-                'name' => 'Atletica',
-                'description' => 'Capacità fisiche, come saltare, arrampicarsi e nuotare.',
-                'peculiar_characteristic' => 'FOR',
-            ],
-            [
-                'name' => 'Cavalcare',
-                'description' => 'Abilità nel montare e controllare animali montati.',
-                'peculiar_characteristic' => 'DEX',
-            ],
-            [
-                'name' => 'Conoscenze Arcane',
-                'description' => 'Conoscenza di creature magiche, piani di esistenza, e misteri arcani.',
-                'peculiar_characteristic' => 'INT',
-            ],
-            [
-                'name' => 'Medicina',
-                'description' => 'Capacità di curare ferite e malattie.',
-                'peculiar_characteristic' => 'WIS',
-            ],
-            [
-                'name' => 'Natura',
-                'description' => 'Conoscenza di piante, animali e fenomeni naturali.',
-                'peculiar_characteristic' => 'INT',
-            ],
-            [
-                'name' => 'Percezione',
-                'description' => 'Capacità di notare dettagli, percepire pericoli e individuare trappole.',
-                'peculiar_characteristic' => 'WIS',
-            ],
-            [
-                'name' => 'Persuasione',
-                'description' => 'Capacità di convincere gli altri e ottenere il loro favore.',
-                'peculiar_characteristic' => 'CHA',
-            ],
-            [
-                'name' => 'Religione',
-                'description' => 'Conoscenza delle religioni, dei pantheon e dei rituali sacri.',
-                'peculiar_characteristic' => 'INT',
-            ],
-            [
-                'name' => 'Sleight of Hand',
-                'description' => 'Abilità nel fare trucchi di prestigio, borseggiare e manipolare oggetti.',
-                'peculiar_characteristic' => 'DEX',
-            ],
-            [
-                'name' => 'Soprannaturale',
-                'description' => 'Conoscenza di creature soprannaturali, piani di esistenza e misteri occulti.',
-                'peculiar_characteristic' => 'INT',
-            ]
-        ];
+return [
+    [
+        'name' => 'Acrobatics',
+        'description' => 'Ability to perform acrobatics and agile movements.',
+        'peculiar_characteristic' => 'DEX',
+    ],
+    [
+        'name' => 'Stealth',
+        'description' => 'Ability to move silently and remain hidden.',
+        'peculiar_characteristic' => 'DEX',
+    ],
+    [
+        'name' => 'History',
+        'description' => 'Knowledge of past events, cultures, and societies.',
+        'peculiar_characteristic' => 'INT',
+    ],
+    [
+        'name' => 'Survival',
+        'description' => 'Skill in finding food, water, and navigating in nature.',
+        'peculiar_characteristic' => 'WIS',
+    ],
+    [
+        'name' => 'Intimidation',
+        'description' => 'Ability to frighten or threaten others.',
+        'peculiar_characteristic' => 'CHA',
+    ],
+    [
+        'name' => 'Deception',
+        'description' => 'Skill in lying or concealing the truth.',
+        'peculiar_characteristic' => 'CHA',
+    ],
+    [
+        'name' => 'Arcana',
+        'description' => 'Knowledge of magical secrets, magical objects, and magical symbols.',
+        'peculiar_characteristic' => 'WIS',
+    ],
+    [
+        'name' => 'Athletics',
+        'description' => 'Physical abilities, such as jumping, climbing, and swimming.',
+        'peculiar_characteristic' => 'FOR',
+    ],
+    [
+        'name' => 'Riding',
+        'description' => 'Ability to ride and control mounted animals.',
+        'peculiar_characteristic' => 'DEX',
+    ],
+    [
+        'name' => 'Arcane Knowledge',
+        'description' => 'Knowledge of magical creatures, planes of existence, and arcane mysteries.',
+        'peculiar_characteristic' => 'INT',
+    ],
+    [
+        'name' => 'Medicine',
+        'description' => 'Ability to heal wounds and diseases.',
+        'peculiar_characteristic' => 'WIS',
+    ],
+    [
+        'name' => 'Nature',
+        'description' => 'Knowledge of plants, animals, and natural phenomena.',
+        'peculiar_characteristic' => 'INT',
+    ],
+    [
+        'name' => 'Perception',
+        'description' => 'Ability to notice details, perceive dangers, and detect traps.',
+        'peculiar_characteristic' => 'WIS',
+    ],
+    [
+        'name' => 'Persuasion',
+        'description' => 'Ability to convince others and gain their favor.',
+        'peculiar_characteristic' => 'CHA',
+    ],
+    [
+        'name' => 'Religion',
+        'description' => 'Knowledge of religions, pantheons, and sacred rituals.',
+        'peculiar_characteristic' => 'INT',
+    ],
+    [
+        'name' => 'Sleight of Hand',
+        'description' => 'Skill in performing tricks, pickpocketing, and manipulating objects.',
+        'peculiar_characteristic' => 'DEX',
+    ],
+    [
+        'name' => 'Supernatural',
+        'description' => 'Knowledge of supernatural creatures, planes of existence, and occult mysteries.',
+        'peculiar_characteristic' => 'INT',
+    ],
+];
